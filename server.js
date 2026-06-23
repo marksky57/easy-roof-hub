86d3b73546761d83860953e8dd9ca3069d3e5964
marksky57 <61353522+marksky57@users.noreply.github.com>
2026-06-22 18:55:19 -0700

    Initialize Express server in server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('.'));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
