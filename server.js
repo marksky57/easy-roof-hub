2089706e1bb528806b8194c3f399dab7a67f7ef3
marksky57 <61353522+marksky57@users.noreply.github.com>
2026-06-22 19:12:03 -0700

    Refactor static file serving and add index route

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static('.'));
+// Serve static files (css, js, assets)
+app.use(express.static(path.join(__dirname)));
+
+// Explicitly serve index.html at root
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const PORT = process.env.PORT || 8080;
 
